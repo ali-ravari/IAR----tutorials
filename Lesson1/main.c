53d77675823cae335420b7cfab595733972bffb1
Ali Ravari <alirezakarbakhshr@gmail.com>
2021-09-12 15:41:40 -0400

    first commit

---
+++ Lesson1/main.c
@@ -0,0 +1,17 @@
+
+int main(){
+  int counter = 0;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  return 0;
+}
